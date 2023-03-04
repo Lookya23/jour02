4905454a77f913d2236c83f1bcdb1d390d9b7bbd
Lookya23 <miguel.platteel@gmail.com>
2023-03-04 02:20:18 +0100

    C00

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -6,14 +6,13 @@
 /*   By: mplattee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 11:32:53 by mplattee          #+#    #+#             */
-/*   Updated: 2023/03/04 02:13:40 by mplattee         ###   ########.fr       */
+/*   Updated: 2023/03/04 02:20:03 by mplattee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 void	ft_putchar(char c)
 {
-	c = 'm';
 	write(1, &c, 1);
 }
 /*int	main(void)
